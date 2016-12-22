0be09c9d31ce1fab99c1ff4bb73ac0e2fb7b4986
Qingqing Sun <qisun@microsoft.com>
2016-12-22 11:12:01 +0800

    move constants to Constants

---
+++ AzureXplatCliJsonToYml/Constants.cs
@@ -0,0 +1,21 @@
+﻿namespace AzureXplatCliJsonToYml
+{
+    using System.Collections.Generic;
+
+    public static class Constants
+    {
+        public const string SourceExtension = ".json";
+        public const string DestExtension = ".xyml";
+        public const string Categories = "categories";
+        public const string Name = "name";
+        public const string Description = "description";
+        public const string Commands = "commands";
+        public const string Usage = "usage";
+
+        public static readonly Dictionary<string, string> ModeNameMapping = new Dictionary<string, string>()
+        {
+            {"arm", "Azure Resource Management"},
+            {"asm", "Azure Service Management"}
+        };
+    }
+}
